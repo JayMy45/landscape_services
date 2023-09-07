f877231d3d453f7ee2159e3c2b2434fb3b363a71
Jeremy Myers <echelonical@gmail.com>
2023-09-06 20:52:04 -0500

    navbar update

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -3,7 +3,7 @@ export default function Navbar() {
     return (
         <>
             <navbar>
-                <div className="navbar flex h-28 border">
+                <div className="navbar flex h-32 border">
                     <div className="w-1/3 border">
 
                     </div>
