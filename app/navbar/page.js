fb2db54a713f77d24a224fac1b71174ca34f13f6
Jeremy Myers <echelonical@gmail.com>
2023-09-06 20:42:27 -0500

    Add layout for navbar

---
+++ app/navbar/page.js
@@ -0,0 +1,20 @@
+export default function Navbar() {
+
+    return (
+        <>
+            <navbar>
+                <div className="navbar flex h-28 border">
+                    <div className="w-1/3 border">
+
+                    </div>
+
+                    <div className="w-1/3 border">
+                    </div>
+
+                    <div className="w-1/3 border">
+                    </div>
+                </div>
+            </navbar>
+        </>
+    )
+}
