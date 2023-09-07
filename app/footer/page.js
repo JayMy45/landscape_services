f61607b36c31964f53d371baf13a0386192c3cc4
Jeremy Myers <echelonical@gmail.com>
2023-09-06 21:28:49 -0500

    setup tentative footer display

---
+++ app/footer/page.js
@@ -0,0 +1,25 @@
+export default function Footer() {
+
+    return (
+        <>
+            <footer>
+                <div className="h-20 w-full border">
+                    <div className="mx-auto border h-full w-96">
+
+                    </div>
+                </div>
+                <div className="footer flex h-48 border">
+                    <div className="w-1/6 border">
+
+                    </div>
+                    <div className="w-2/6 border">
+
+                    </div>
+                    <div className="w-3/6 border">
+
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
