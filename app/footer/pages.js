8d314a06f85d28fbe41966e0b41104f966bfc58c
Jeremy Myers <echelonical@gmail.com>
2023-09-06 20:57:37 -0500

    footer layout

---
+++ app/footer/pages.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+    return (
+        <>
+            <footer>
+                <div className="footer flex h-32 border">
+                </div>
+            </footer>
+        </>
+    )
+}
