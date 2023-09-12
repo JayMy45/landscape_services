c00a391af4e398799e2bd6900c5212481fbc4e7f
Jeremy Myers <echelonical@gmail.com>
2023-09-11 21:25:15 -0500

    section created.

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,6 +4,11 @@ export default function Contact() {
     return (
         <>
             <h1>Contact a Pimp...</h1>
+            <section>
+                <div className="h-screen border">
+
+                </div>
+            </section>
         </>
     )
 }
