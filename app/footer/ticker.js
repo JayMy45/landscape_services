1861e337409f86d9658c944f40b184c7946699c9
Jeremy Myers <echelonical@gmail.com>
2023-09-14 22:09:25 -0500

    css/tailwind updates

--- app/footer/ticker.js
+++ app/footer/ticker.js
@@ -24,12 +24,12 @@ export default function TickerComponent() {
             backgroundSize: "300px 300px",
             backgroundPosition: "center",
             backgroundRepeat: "repeat"
-        }} className='my-auto p-1 drop-shadow-lg border-t-2 border-b-2 border-amber-100 dark:border-black'>
+        }} className='my-auto p-1 drop-shadow-lg border-t-2 border-b-2 border-zinc-100 dark:border-black'>
             <div ref={ref} className="ticker" style={{ display: 'inline-flex', whiteSpace: 'nowrap' }}>
-                <div style={{ paddingRight: '0px' }} className='text-yellow-300 dark:text-blue-300'>
+                <div style={{ paddingRight: '0px' }} className='text-zinc-100 dark:text-blue-200'>
                     &bull; The starting price for mowing/trimming is $85 (no matter the lot size) &bull; Any extra services Charges will be applied &bull; We accept Cash, Cashapp, Visa Debit/Credit Cards &bull; Late charges will apply when not paid immediately after service &bull; Deposits are required before service for new potential clients
                 </div>
-                <div className='text-yellow-300 dark:text-blue-300'>
+                <div className='text-zinc-100 dark:text-blue-200'>
                     &bull; The starting price for mowing/trimming is $85 (no matter the lot size) &bull; Any extra services Charges will be applied &bull; We accept Cash, Cashapp, Visa Debit/Credit Cards &bull; Late charges will apply when not paid immediately after service &bull; Deposits are required before service for new potential clients
                 </div>
             </div>
