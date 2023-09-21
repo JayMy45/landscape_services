957d8a99dc50adc425d6c98b971b6be887939140
Jeremy Myers <echelonical@gmail.com>
2023-09-20 20:42:54 -0500

    implemented carousel

--- app/carousel/page.js
+++ app/carousel/page.js
@@ -9,49 +9,95 @@ export default function CarouselPage() {
     return (
         <>
             <Carousel
-                className="mb-10"
+                className=""
                 animation="slide"
                 indicators={false}
+                timeout={1000}
                 height={300}
                 swipe={true}
                 next={(next, active) => console.log(`we left ${active}, and are now at ${next}`)}
                 prev={(prev, active) => console.log(`we left ${active}, and are now at ${prev}`)}
             >
                 <div className="flex justify-center items-center h-full">
-                    <Paper className=" border-r">
-                        <img src="lawnPics/Residential1a.png" alt="Flower Bed" className='bg-white' height={750} width={750} />
+                    <Paper className=" border-r hidden md:block">
+                        <img src="lawnPics/Residential1a.png" alt="Residential Property 1" className='bg-white' height={750} width={750} />
                     </Paper>
                     <Paper className="bg-white">
-                        <img src="lawnPics/Residential1.png" alt="Flower Bed" className='bg-white' height={750} width={750} />
+                        <img src="lawnPics/Residential1.png" alt="Residential Property 1a" className='bg-white' height={750} width={750} />
                     </Paper>
                 </div>
 
                 <div className="flex justify-center items-center h-full">
-                    <Paper className=" border-r">
-                        <img src="lawnPics/Residential2.png" alt="Flower Bed" className='bg-white' height={750} width={750} />
+                    <Paper className="md:border-r">
+                        <img src="lawnPics/Residential2.png" alt="Residential Property" className='bg-white' height={750} width={750} />
                     </Paper>
-                    <Paper className="bg-white">
-                        <img src="lawnPics/Residential2a.png" alt="Flower Bed" className='bg-white' height={750} width={750} />
-                    </Paper>
-                </div>
-
-                <div className="flex justify-center items-center h-full">
-                    <Paper className=" border-r">
-                        <img src="lawnPics/Residential3.png" alt="Flower Bed" className='bg-white' height={750} width={750} />
-                    </Paper>
-                    <Paper className="bg-white">
-                        <img src="lawnPics/Residential3a.png" alt="Flower Bed" className='bg-white' height={750} width={750} />
+                    <Paper className="bg-white hidden md:block">
+                        <img src="lawnPics/Residential2a.png" alt="Residential Property" className='bg-white' height={750} width={750} />
                     </Paper>
                 </div>
 
                 <div className="flex justify-center items-center h-full">
-                    <Paper className=" border-r">
-                        <img src="lawnPics/Commercial1.png" alt="Flower Bed" className='bg-white' height={750} width={750} />
+                    <Paper className=" border-r hidden md:block">
+                        <img src="lawnPics/Residential3.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                    <Paper className="bg-white">
+                        <img src="lawnPics/Residential3a.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                </div>
+
+                <div className="flex justify-center items-center h-full">
+                    <Paper className=" border-r hidden md:block">
+                        <img src="lawnPics/resident5.png" alt="Residential Property" className='bg-white' height={750} width={750} />
                     </Paper>
                     <Paper className="">
-                        <img src="lawnPics/Commercial1a.png" alt="Flower Bed" className='bg-white' height={750} width={750} />
+                        <img src="lawnPics/resident5a.png" alt="Residential Property" className='bg-white' height={750} width={750} />
                     </Paper>
                 </div>
+
+                <div className="flex justify-center items-center h-full">
+                    <Paper className=" border-r hidden md:block">
+                        <img src="lawnPics/resident6.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                    <Paper className="">
+                        <img src="lawnPics/resident6a.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                </div>
+
+                <div className="flex justify-center items-center h-full">
+                    <Paper className="md:border-r">
+                        <img src="lawnPics/resident7.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                    <Paper className=" hidden md:block">
+                        <img src="lawnPics/resident7a.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                </div>
+
+                <div className="flex justify-center items-center h-full">
+                    <Paper className="border-r hidden md:block">
+                        <img src="lawnPics/Commercial1.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                    <Paper className="">
+                        <img src="lawnPics/Commercial1a.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                </div>
+
+                <div className="flex justify-center items-center h-full">
+                    <Paper className="border-r hidden md:block">
+                        <img src="lawnPics/Commercial3a.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                    <Paper className="">
+                        <img src="lawnPics/Commercial3.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                </div>
+
+                {/* <div className="flex justify-center items-center h-full">
+                    <Paper className="border-r ">
+                        <img src="lawnPics/Commercial4.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                    <Paper className="hidden md:block">
+                        <img src="lawnPics/Commercial44.png" alt="Residential Property" className='bg-white' height={750} width={750} />
+                    </Paper>
+                </div> */}
 
             </Carousel>
         </>
--- app/page.js
+++ app/page.js
@@ -3,10 +3,15 @@ import Contact from './contact/page'
 import Services from './services/page'
 import AreaPage from './area/page'
 import About from './about/page'
+import CarouselPage from './carousel/page'
 
 export default function Home() {
   return (
     <main className="min-h-screen md:px-24 md:pt-5 md:pb-5">
+      <div className='flex justify-center bg-black bg-opacity-30 rounded-t-xl w-full'>
+        <Image src="/JonesLandService2.png" height={200} width={350} alt='Jones Landscaping Services' />
+      </div>
+      <CarouselPage />
       <About />
       <AreaPage />
       <Services />
