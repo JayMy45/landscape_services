3d4bf625bec45f0a5e8cbfbad25c41d42f451a92
Jeremy Myers <echelonical@gmail.com>
2023-09-28 20:40:23 -0500

    updated footer with links on smaller screens for navigation ease

--- app/footer/page.js
+++ app/footer/page.js
@@ -15,59 +15,62 @@ export default function Footer() {
             </div>
             <footer className=" bg-stone-400 dark:bg-stone-700 bg-opacity-95 dark:bg-opacity-95 pt-2 shadow-md">
 
-                <div className="footer flex h-44 px-5 mx-auto">
+                <div className="footer flex flex-row h-36 md:h-44 px-5 mx-auto">
 
                     <div className="w-5/12 md:w-3/12 lg:w-3/12 flex justify-center items-center">
                         <Image className="hidden md:block" src="/jlsLOGO1.png" alt="Business Logo" width={250} height={250} />
                         <Image className="block md:hidden px-1 mx-auto" src="/jlsLOGO.png" alt="Business Logo with initial" width={75} height={75} />
                     </div>
 
-                    <div className="hidden md:block text-center ml-10 md:w-6/12 lg:w-7/12">
-                        <div className="py-4 ">
-                            <ul className="inline-flex md:justify-between lg:justify-evenly items-center py-16 md:w-80 lg:w-full">
-                                <Link href="/">
-                                    <li className="hover:border-b border-blue-50 p-1 text-blue-50">Home</li>
-                                </Link>
-                                <Link href="#about">
-                                    <li className="hidden md:block hover:border-b border-blue-50 p-1 text-blue-50">About</li>
-                                    <li className="block md:hidden hover:border-b border-blue-50 p-1 text-blue-50">About Us</li>
-                                </Link>
-                                <Link href="#area">
-                                    <li className="hover:border-b border-blue-50 p-1 text-blue-50">Areas</li>
-                                </Link>
-                                <Link href="#services">
-                                    <li className="hover:border-b border-blue-50 p-1 text-blue-50">Services</li>
-                                </Link>
-                                <Link href="#contact">
-                                    <li className="hidden md:block hover:border-b border-blue-50 p-1 text-blue-50">Contact</li>
-                                    <li className="block md:hidden hover:border-b border-blue-50 p-1 text-blue-50">Contact Us</li>
-                                </Link>
+                    <div className="w-7/12 md:w-9/12 lg:w-9/12 h-1/2 md:h-full flex flex-col md:flex-row">
+
+                        <div className="h-fit md:h-10 text-center md:ml-10 w-12/12 md:w-8/12">
+                            <div className="py-1 md:py-4 text-xs md:text-sm">
+                                <ul className="inline-flex md:justify-between lg:justify-evenly items-center py-1 md:py-16 md:w-80 lg:w-full">
+                                    <Link href="/">
+                                        <li className="hover:border-b border-blue-50 p-1 text-blue-700 md:text-blue-50">Home</li>
+                                    </Link>
+                                    <Link href="#about">
+                                        <li className="block lg:hidden hover:border-b border-blue-50 p-1 text-blue-700 md:text-blue-50">About</li>
+                                        <li className="hidden lg:block hover:border-b border-blue-50 p-1 text-blue-700 md:text-blue-50">About Us</li>
+                                    </Link>
+                                    <Link href="#area">
+                                        <li className="hover:border-b border-blue-50 p-1 text-blue-700 md:text-blue-50">Areas</li>
+                                    </Link>
+                                    <Link href="#services">
+                                        <li className="hover:border-b border-blue-50 p-1 text-blue-700 md:text-blue-50">Services</li>
+                                    </Link>
+                                    <Link href="#contact" className="hidden md:block">
+                                        <li className="block lg:hidden hover:border-b border-blue-50 p-1 text-blue-700 md:text-blue-50">Contact</li>
+                                        <li className="hidden lg:block hover:border-b border-blue-50 p-1 text-blue-700 md:text-blue-50">Contact Us</li>
+                                    </Link>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="hidden lg:block lg:w-4/12 text-left">
+                            <ul className="flex flex-col items-center h-1/2 md:h-full py-16">
+
+                                <li className="text-sm md:text-xs xl:text-sm border-blue-50 md:p-1 text-blue-50">Phone: 803-563-0416</li>
+                                <li className="text-sm md:text-xs xl:text-sm border-blue-50 md:p-1 text-blue-50">Email:<span> </span>
+                                    <Link href="contact" className="hover:border-b">
+                                        jonesservice85@gmail.com
+                                    </Link>
+                                </li>
                             </ul>
                         </div>
-                    </div>
 
-                    <div className="hidden lg:block lg:w-3/12 text-left">
-                        <ul className="flex flex-col items-center h-full py-16">
+                        <div className="lg:hidden block w-12/12 md:w-4/12 lg:w-3/12 text-left md:mt-5 md:py-10">
+                            <div className="flex flex-col items-center h-fit md:h-full">
 
-                            <li className="text-sm md:text-xs xl:text-sm border-blue-50 md:p-1 text-blue-50">Phone: 803-563-0416</li>
-                            <li className="text-sm md:text-xs xl:text-sm border-blue-50 md:p-1 text-blue-50">Email:<span> </span>
-                                <Link href="contact" className="hover:border-b">
-                                    jonesservice85@gmail.com
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="lg:hidden block w-5/12 md:w-3/12 lg:w-3/12 text-left">
-                        <div className="flex flex-col items-center h-full py-16">
-
-                            <h2 className="block md:hidden text-md font-bold border-blue-50 md:p-1 text-blue-50 uppercase"><u>Contact</u></h2>
-                            <h2 className="text-sm xl:text-sm border-blue-50 md:p-1 text-blue-50">803-563-0416</h2>
-                            <h2 className="text-sm xl:text-sm border-blue-50 md:p-1 text-blue-50">
-                                <Link href="contact" className="hover:border-b">
-                                    jonesservice85@gmail.com
-                                </Link>
-                            </h2>
+                                <h2 className="block md:hidden text-md font-bold border-blue-50 md:p-1 text-blue-50 uppercase"><u>Contact</u></h2>
+                                <h2 className="text-sm xl:text-sm border-blue-50 md:p-1 text-blue-50">803-563-0416</h2>
+                                <h2 className="text-sm xl:text-sm border-blue-50 md:p-1 text-blue-50">
+                                    <Link href="contact" className="hover:border-b">
+                                        jonesservice85@gmail.com
+                                    </Link>
+                                </h2>
+                            </div>
                         </div>
                     </div>
 
