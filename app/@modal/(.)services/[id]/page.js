a2783bb85e351dd268c6ae4326ca08ecfdd38865
Jeremy Myers <echelonical@gmail.com>
2023-10-09 23:50:20 -0500

    refactored service/[id] page with imports from serviceData and correct functionality

--- app/@modal/(.)services/[id]/page.js
+++ app/@modal/(.)services/[id]/page.js
@@ -1,14 +1,15 @@
 import Frame from '@/components/frame/frame'
 import Modal from '@/components/modal/modal'
-import swagPhotos, { Photo } from '../../../../photos'
+import { serviceData } from '@/app/data/serviceData';
 
-export default function PhotoModal({ params: { id: photoId } }) {
-    const photos = swagPhotos;
-    const photo = photos.find((p) => p.id === photoId);
+export default function PhotoModal({ params: { id: serviceId } }) {
+
+    const services = serviceData;
+    const service = services.find((p) => p.id === serviceId);
 
     return (
         <Modal>
-            <Frame photo={photo} />
+            <Frame service={service} />
         </Modal>
     )
 }
